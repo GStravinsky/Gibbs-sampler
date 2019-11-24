29111959239e5cd38a6fcc59d632b05dacc2e8cb
Gabrielė Stravinskaitė <stravinskaite.g@gmail.com>
2019-11-24 15:38:35 +0000

    updated code

--- Gibbs_data.R
+++ Gibbs_data.R
@@ -1,51 +1,45 @@
+data <- read.table("~/Desktop/data.txt", quote="\"", comment.char=a)
 
-if(!require(invgamma)){
-  install.packages("invgamma")
-  library(invgamma)
-}
-
-if(!require(MASS)){
-  install.packages("MASS")
-  library(MASS)
-}
-
-
-
-###### GENERATE DUMMY DATA #######
-y <- rnorm(10)
-X <- matrix(data = rnorm(20), ncol = 2, nrow = 10)
-n <- 10
+# PREPARE DATA #
+summary(data) # V11 is the dependent variable
+y <- data$V11
+n <- nrow(data)
+X <- data[,c(1:10)]
+Int <- rep(1, n)
+X$Int <- Int
+X <- as.matrix(sapply(X, as.numeric))
 
 #### DEFINE THE CHARARCTERISTICS OF SIGMA DISTRIBUTION #####
 g <- 1 # for simplicity
 XX <- t(X) %*% X  
-beta_hat <- solve(XX) %*% t(X) %*% y # MLE/OLS solution
-beta_mean <- c(0,0) # arbitrary choice of the hyperparameter
+beta_hat <- solve(XX, t(X) %*% y) # MLE/OLS solution
+beta0 <- rep(0, 11) # arbitrary choice of the hyperparameter
 resid_sigma <- (t(y-X %*% beta_hat) %*% (y-X %*% beta_hat))^2 # s^2
 shape_sigma <-  n/2
-scale_sigma <- resid_sigma/2 + 1/(2*(g+1)) %*% t(beta_mean-beta_hat) %*% XX %*% (beta_mean-beta_hat)
+scale_sigma <- resid_sigma/2 + 1/(2*(g+1)) %*% t(beta0-beta_hat) %*% XX %*% (beta0-beta_hat)
+sigma <- rinvgamma(1, shape = shape_sigma, rate = scale_sigma)
 
 ###### DEFINE THE MOMENTS OF BETA DISTRIBUTION #######
-beta_mean <- g/(g+1) * (beta_mean/g + beta_hat)
-beta_sd <- sqrt((sigma * g)/(g+1) * XX)
+beta_mean <- g/(g+1) * (beta0/g + beta_hat)
+beta_sd <- (sigma * g)/(g+1) * XX
 
 set.seed(1620789)
 
 ###### GIBBS SAMPLER #######
-gibbs <- function(T, m){
+gibbs <- function(T, b){
   # T - number of iterations
   # m - number of initial iterations to be omitted from the final result - burnin
   result <- matrix(ncol = 3, nrow = T)
   for (i in 1: T) {
-    sigma <- rinvgamma(1, shape = shape_sigma, scale = scale_sigma)
-    beta_sd <- sqrt((sigma * g)/(g+1) * XX)
+    sigma <- rinvgamma(1, shape = shape_sigma, rate = scale_sigma)
+    beta_sd <- (sigma * g)/(g+1) * XX
     beta <- mvrnorm(1, beta_mean, beta_sd)
     result[i, ] <- c(beta[1], beta[2], sigma)
   }
-  adjusted_result <- result[c(m:T),]
+  adjusted_result <- result[c(b:T),]
 }
 
-test_output <- gibbs(1000)
+test_output <- gibbs(T=10000, b=200)
 
 # performance plots
 par(mfrow=c(3,2))
@@ -65,4 +59,3 @@ par(mfrow=c(1,1))
 
 
 
-              
